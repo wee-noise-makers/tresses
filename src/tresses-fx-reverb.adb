7c398709fdde19a1027c51a2feb3fd5e473a2120
Fabien Chouteau <fabien.chouteau@gmail.com>
2023-04-15 07:42:06 +0200

    Improve the reverb

--- src/tresses-fx-reverb.adb
+++ src/tresses-fx-reverb.adb
@@ -1,17 +1,16 @@
+--
+--  SPDX-License-Identifier: MIT
+--
+--  Based on Clouds from Mutable Instruments:
+--  Copyright 2014 Emilie Gillet.
+
 with Tresses.DSP;
 
 package body Tresses.FX.Reverb is
 
-   type DL is record
-      Base : U16;
-      Len  : U16;
-   end record;
-
    type Context is tagged record
       Accumulator : S16 := 0;
       Prev_Read : S16 := 0;
-      LFO_Value_1 : S16 := 0;
-      LFO_Value_2 : S16 := 0;
       Write_Ptr : U16 := 0;
    end record;
 
@@ -26,19 +25,22 @@ package body Tresses.FX.Reverb is
         S16 ((S32 (Value) * S32 (Gain) / 2**15));
    end Read;
 
-   procedure Read_Tail (This : in out Context;
-                        Buffer : Delay_Buffer;
-                        D : DL;
-                        Scale : S16)
+   procedure Read_Offset (This : in out Context;
+                          Buffer : Reverb_Buffer;
+                          Offset : U16;
+                          Scale : S16)
    is
-      Index : constant U16 :=
-        (This.Write_Ptr + D.Base + D.Len - 1) mod Buffer'Length;
+      Index : constant U16 := (This.Write_Ptr + Offset) mod Buffer'Length;
+      Acc : S32;
    begin
       This.Prev_Read := Buffer (Index);
 
-      This.Accumulator := This.Accumulator +
-        S16 ((S32 (This.Prev_Read) * S32 (Scale) / 2**15));
-   end Read_Tail;
+      Acc := S32 (This.Accumulator);
+
+      Acc := Acc + (S32 (This.Prev_Read) * S32 (Scale) / 2**15);
+
+      This.Accumulator := S16 (DSP.Clip_S16 (Acc));
+   end Read_Offset;
 
    procedure Write (This : Context; Value : out S16) is
    begin
@@ -52,46 +54,33 @@ package body Tresses.FX.Reverb is
         S16 ((S32 (This.Accumulator) * S32 (Scale)) / 2**15);
    end Write_Scale;
 
-   procedure Write_All_Pass (This : in out Context;
-                             Buffer : in out Delay_Buffer;
-                             D : DL;
-                             Scale : S16)
+   procedure Write_All_Pass (This   : in out Context;
+                             Buffer : in out Reverb_Buffer;
+                             Offset :        U16;
+                             Scale  :        S16)
    is
       Index : constant U16 :=
-        (This.Write_Ptr + D.Base) mod Buffer'Length;
+        (This.Write_Ptr + Offset) mod Buffer'Length;
+      Acc : S32;
    begin
       Buffer (Index) := This.Accumulator;
 
-      This.Accumulator :=
-        S16 ((S32 (This.Accumulator) * S32 (Scale)) / 2**15);
+      Acc := (S32 (This.Accumulator) * S32 (Scale)) / 2**15;
+      Acc := Acc + S32 (This.Prev_Read);
 
-      This.Accumulator := This.Accumulator + This.Prev_Read;
+      This.Accumulator := S16 (DSP.Clip_S16 (Acc));
    end Write_All_Pass;
 
-   procedure Write (This : in out Context;
-                    Buffer : in out Delay_Buffer;
-                    D : DL;
-                    Scale : S16)
+   procedure Write_Double (This   : in out Context;
+                           Buffer : in out Reverb_Buffer;
+                           Offset :        U16)
    is
       Index : constant U16 :=
-        (This.Write_Ptr + D.Base) mod Buffer'Length;
+        (This.Write_Ptr + Offset) mod Buffer'Length;
    begin
       Buffer (Index) := This.Accumulator;
 
-      This.Accumulator :=
-        S16 ((S32 (This.Accumulator) * S32 (Scale)) / 2**15);
-   end Write;
-
-   procedure Write_Double (This : in out Context;
-                           Buffer : in out Delay_Buffer;
-                           D : DL)
-   is
-      Index : constant U16 :=
-        (This.Write_Ptr + D.Base) mod Buffer'Length;
-   begin
-      Buffer (Index) := This.Accumulator;
-
-      This.Accumulator := This.Accumulator * 2;
+      This.Accumulator := S16 (DSP.Clip_S16 (S32 (This.Accumulator) * 2));
    end Write_Double;
 
    procedure LP (This : in out Context;
@@ -105,17 +94,6 @@ package body Tresses.FX.Reverb is
       This.Accumulator := State;
    end LP;
 
-   procedure HP (This : in out Context;
-                 State : in out S16;
-                 Coef : S16)
-   is
-   begin
-      State := State +
-        S16 ((S32 (Coef) * S32 (This.Accumulator - State)) / 2**15);
-
-      This.Accumulator := This.Accumulator - State;
-   end HP;
-
    procedure Start (This   : in out Instance;
                     C      : in out Context)
    is
@@ -128,11 +106,6 @@ package body Tresses.FX.Reverb is
       C.Accumulator := 0;
       C.Prev_Read := 0;
       C.Write_Ptr := U16 (This.Write_Ptr);
-
-      if (U16 (C.Write_Ptr) and 31) /= 0 then
-         C.LFO_Value_1 := This.LFO_1.Render;
-         C.LFO_Value_2 := This.LFO_2.Render;
-      end if;
    end Start;
 
    -----------
@@ -142,15 +115,52 @@ package body Tresses.FX.Reverb is
    procedure Reset (This : in out Instance) is
    begin
       This.Buffer := (others => 0);
-
-      This.LFO_1.Set_Shape (LFO.Sine);
-      This.LFO_1.Set_Loop_Mode (LFO.Repeat);
-      This.LFO_1.Set_Rate (Param_Range'Last / 3);
-
-      This.LFO_2.Set_Shape (LFO.Sine);
-      This.LFO_2.Set_Loop_Mode (LFO.Repeat);
-      This.LFO_2.Set_Rate (Param_Range'Last / 4);
    end Reset;
+
+   ----------------
+   -- Set_Amount --
+   ----------------
+
+   procedure Set_Amount (This : in out Instance; V : Param_Range) is
+   begin
+      This.Amount := V;
+   end Set_Amount;
+
+   --------------
+   -- Set_Gain --
+   --------------
+
+   procedure Set_Gain (This : in out Instance; V : Param_Range) is
+   begin
+      This.Input_Gain := V;
+   end Set_Gain;
+
+   --------------
+   -- Set_Time --
+   --------------
+
+   procedure Set_Time (This : in out Instance; V : Param_Range) is
+   begin
+      This.Reverb_Time := V;
+   end Set_Time;
+
+   -------------------
+   -- Set_Diffusion --
+   -------------------
+
+   procedure Set_Diffusion (This : in out Instance; V : Param_Range) is
+   begin
+      This.Diffusion := V;
+   end Set_Diffusion;
+
+   ------------------
+   -- Set_Low_Pass --
+   ------------------
+
+   procedure Set_Low_Pass (This : in out Instance; V : Param_Range) is
+   begin
+      This.LP := V;
+   end Set_Low_Pass;
 
    -------------
    -- Process --
@@ -160,29 +170,29 @@ package body Tresses.FX.Reverb is
                       Left  : in out Mono_Buffer;
                       Right : in out Mono_Buffer)
    is
-      Ap1   : constant DL := (0, 113);
-      Ap2   : constant DL := (Ap1.Base + Ap1.Len + 1, 162);
-      Ap3   : constant DL := (Ap2.Base + Ap2.Len + 1, 241);
-      Ap4   : constant DL := (Ap3.Base + Ap3.Len + 1, 399);
-      Dap1a : constant DL := (Ap4.Base + Ap4.Len + 1, 1653);
-      Dap1b : constant DL := (Dap1a.Base + Dap1a.Len + 1, 2038);
-      Del1  : constant DL := (Dap1b.Base + Dap1b.Len + 1, 3411);
-      Dap2a : constant DL := (Del1.Base + Del1.Len + 1, 1913);
-      Dap2b : constant DL := (Dap2a.Base + Dap2a.Len + 1, 1663);
-      Del2  : constant DL := (Dap2b.Base + Dap2b.Len + 1, 4782);
 
       C : Context;
 
-      Kap : constant S16 := S16 (This.Diffusion);
-      Klp : constant S16 := S16 (This.LP);
-      Krt : constant S16 := S16 (This.Reverb_Time);
+      Kap    : constant S16 := S16 (This.Diffusion);
+      Klp    : constant S16 := S16 (This.LP);
+      Krt    : constant S16 := S16 (This.Reverb_Time);
       Amount : constant S16 := S16 (This.Amount);
-      Gain : constant S16 := S16 (This.Input_Gain);
+      Gain   : constant S16 := S16 (This.Input_Gain);
 
       LP1 : S16 renames This.LP_Decay_1;
       LP2 : S16 renames This.LP_Decay_1;
 
    begin
+
+      --  This implementation is based on reverb.h from the eurorack
+      --  repository of Mutable instruments:
+      --  https://github.com/pichenettes/eurorack/blob/master/clouds/dsp/fx/reverb.h
+      --
+      --  Itself based on the 1997 paper "Effect Design: Part
+      --  1: Reverberator and Other Filters" by Jon Dattorro:
+      --  https://ccrma.stanford.edu/~dattorro/EffectDesignPart1.pdf
+      --
+      --  Compared to the MI's version this reverb doesn't have LFOs.
 
       for Index in Left'Range loop
          declare
@@ -193,51 +203,54 @@ package body Tresses.FX.Reverb is
          begin
             Start (This, C);
 
-            --  Smear AP1 inside the loop.
-            --  TODO: Interpolate (Ap1, 10.0, C.LFO_Value_1, 10.0, 1.0);
-            --  TODO: c.Write(ap1, 100, 0.0f);
-
             C.Read (S16 (DSP.Clip_S16 (S32 (L) + S32 (R))), Gain);
 
             --  Diffuse through 4 allpasses
-            C.Read_Tail (This.Buffer, Ap1, Kap);
-            C.Write_All_Pass (This.Buffer, Ap1, -Kap);
-            C.Read_Tail (This.Buffer, Ap2, Kap);
-            C.Write_All_Pass (This.Buffer, Ap2, -Kap);
-            C.Read_Tail (This.Buffer, Ap3, Kap);
-            C.Write_All_Pass (This.Buffer, Ap3, -Kap);
-            C.Read_Tail (This.Buffer, Ap4, Kap);
-            C.Write_All_Pass (This.Buffer, Ap4, -Kap);
+            C.Read_Offset (This.Buffer, Ap1_Tail, Kap);
+            C.Write_All_Pass (This.Buffer, Ap1_Base, -Kap);
+
+            C.Read_Offset (This.Buffer, Ap2_Tail, Kap);
+            C.Write_All_Pass (This.Buffer, Ap2_Base, -Kap);
+
+            C.Read_Offset (This.Buffer, Ap3_Tail, Kap);
+            C.Write_All_Pass (This.Buffer, Ap3_Base, -Kap);
+
+            C.Read_Offset (This.Buffer, Ap4_Tail, Kap);
+            C.Write_All_Pass (This.Buffer, Ap4_Base, -Kap);
+
             C.Write (Apout);
 
-            --  Main reverb loop
+            --  Left: 2 allpasses + Delay
             C.Load (Apout);
-            --  TODO: c.Interpolate(del2, 4680.0f, LFO_2, 100.0f, krt);
-
-            C.Read_Tail (This.Buffer, Del1, Krt); -- TODO: Remove?
 
             C.LP (LP1, Klp);
-            C.Read_Tail (This.Buffer, Dap1a, -Kap);
-            C.Write_All_Pass (This.Buffer, Dap1a, Kap);
-            C.Read_Tail (This.Buffer, Dap1b, Kap);
-            C.Write_All_Pass (This.Buffer, Dap1b, -Kap);
-            C.Write_Double (This.Buffer, Del1); -- TODO: Scale 2.0
-            C.Write_Scale (Wet, 0); --  TODO: 0.0???
+            C.Read_Offset (This.Buffer, Del2_Tail, Krt);
 
-            L := L + S16((S32 (Wet - L) * S32 (Amount)) / 2**15);
+            C.Read_Offset (This.Buffer, Dap1a_Tail, -Kap);
+            C.Write_All_Pass (This.Buffer, Dap1a_Base, Kap);
+            C.Read_Offset (This.Buffer, Dap1b_Tail, Kap);
+            C.Write_All_Pass (This.Buffer, Dap1b_Base, -Kap);
+            C.Write_Double (This.Buffer, Del1_Base);
+            C.Write_Scale (Wet, 0);
 
+            L := L +
+              S16 (DSP.Clip_S16 ((S32 (Wet - L) * S32 (Amount)) / 2**15));
+
+            --  Right: 2 allpasses + Delay
             C.Load (Apout);
-            --  TODO: c.Interpolate(del2, 4680.0f, LFO_2, 100.0f, krt);
-            C.LP (LP2, Klp);
-            C.Read_Tail (This.Buffer, Del1, Krt);
-            C.Read_Tail (This.Buffer, Dap2a, Kap);
-            C.Write_All_Pass (This.Buffer, Dap2a, -Kap);
-            C.Read_Tail (This.Buffer, Dap2b, -Kap);
-            C.Write_All_Pass (This.Buffer, Dap2b, Kap);
-            C.Write_Double (This.Buffer, Del2); -- TODO: Scale 2.0
-            C.Write_Scale (Wet, 0); --  TODO: 0.0???
 
-            R := R + S16((S32 (Wet - R) * S32 (Amount)) / 2**15);
+            C.LP (LP2, Klp);
+            C.Read_Offset (This.Buffer, Del1_Tail, Krt);
+
+            C.Read_Offset (This.Buffer, Dap2a_Tail, Kap);
+            C.Write_All_Pass (This.Buffer, Dap2a_Base, -Kap);
+            C.Read_Offset (This.Buffer, Dap2b_Tail, -Kap);
+            C.Write_All_Pass (This.Buffer, Dap2b_Base, Kap);
+            C.Write_Double (This.Buffer, Del2_Base);
+            C.Write_Scale (Wet, 0);
+
+            R := R +
+              S16 (DSP.Clip_S16 ((S32 (Wet - R) * S32 (Amount)) / 2**15));
          end;
       end loop;
    end Process;
--- src/tresses-fx-reverb.ads
+++ src/tresses-fx-reverb.ads
@@ -1,33 +1,114 @@
-with Tresses.FX.Delay_Line; use Tresses.FX.Delay_Line;
+--
+--  SPDX-License-Identifier: MIT
+--
+--  Based on Clouds from Mutable Instruments:
+--  Copyright 2014 Emilie Gillet.
 
-with Tresses.LFO;
+with Tresses.Resources;
 
+generic
+   --  Size of each delay line in number of samples. The default value are
+   --  based on the Mutable Instruments constants and adapted for the sample
+   --  rate.
+   --
+   --  MI_Len : Length of delay in Mutable Instruments' implementation
+   --  MI_SR  : Sample rate of Mutable Instruments's implementation
+   --  TS_SR  : Tresses sample rate
+   --
+   --  Lenght = (MI_Len / MI_SR) * TS_SR
+   --
+   --  E.g: (113 / 32_000) * 44_100 ~= 156
+   --  https://github.com/pichenettes/eurorack/blob/master/clouds/dsp/fx/reverb.h#L56
+   --
+   --  The total delay line samples in MI is 16375. At 44.1kHz sample rate
+   --  that means: (16375 / 32_000) * 44_100 ~= 22567 sample -> 45_134 Bytes
+   --  of memory footprint for the reverb.
+
+
+   --  4 x All-Pass
+   Ap1_Len   : U16 := U16 ((113.0 / 32_000.0) * Resources.SAMPLE_RATE_REAL);
+   Ap2_Len   : U16 := U16 ((162.0 / 32_000.0) * Resources.SAMPLE_RATE_REAL);
+   Ap3_Len   : U16 := U16 ((241.0 / 32_000.0) * Resources.SAMPLE_RATE_REAL);
+   Ap4_Len   : U16 := U16 ((399.0 / 32_000.0) * Resources.SAMPLE_RATE_REAL);
+
+   --  2 x (2 x All-Pass + Delay)
+   Dap1a_Len : U16 := U16 ((1653.0 / 32_000.0) * Resources.SAMPLE_RATE_REAL);
+   Dap1b_Len : U16 := U16 ((2038.0 / 32_000.0) * Resources.SAMPLE_RATE_REAL);
+   Del1_Len  : U16 := U16 ((3411.0 / 32_000.0) * Resources.SAMPLE_RATE_REAL);
+
+   Dap2a_Len : U16 := U16 ((1913.0 / 32_000.0) * Resources.SAMPLE_RATE_REAL);
+   Dap2b_Len : U16 := U16 ((1663.0 / 32_000.0) * Resources.SAMPLE_RATE_REAL);
+   Del2_Len  : U16 := U16 ((4782.0 / 32_000.0) * Resources.SAMPLE_RATE_REAL);
 package Tresses.FX.Reverb
 with Preelaborate
 is
 
+   type Instance is private;
+
+   procedure Reset (This : in out Instance);
+
+   procedure Set_Amount (This : in out Instance; V : Param_Range);
+   procedure Set_Gain (This : in out Instance; V : Param_Range);
+   procedure Set_Time (This : in out Instance; V : Param_Range);
+   procedure Set_Diffusion (This : in out Instance; V : Param_Range);
+   procedure Set_Low_Pass (This : in out Instance; V : Param_Range);
+
+   procedure Process (This  : in out Instance;
+                      Left  : in out Mono_Buffer;
+                      Right : in out Mono_Buffer);
+
+private
+
+   --  Indexes for the first and last element of each delay line
+   Ap1_Base   : constant U16 := 0;
+   Ap1_Tail   : constant U16 := Ap1_Base + Ap1_Len - 1;
+
+   Ap2_Base   : constant U16 := Ap1_Tail + 1;
+   Ap2_Tail   : constant U16 := Ap2_Base + Ap2_Len - 1;
+
+   Ap3_Base   : constant U16 := Ap2_Tail + 1;
+   Ap3_Tail   : constant U16 := Ap3_Base + Ap3_Len - 1;
+
+   Ap4_Base   : constant U16 := Ap3_Tail + 1;
+   Ap4_Tail   : constant U16 := Ap4_Base + Ap4_Len - 1;
+
+   Dap1a_Base : constant U16 := Ap4_Tail + 1;
+   Dap1a_Tail : constant U16 := Dap1a_Base + Dap1a_Len - 1;
+
+   Dap1b_Base : constant U16 := Dap1a_Tail + 1;
+   Dap1b_Tail : constant U16 := Dap1b_Base + Dap1b_Len - 1;
+
+   Del1_Base  : constant U16 := Dap1b_Tail + 1;
+   Del1_Tail  : constant U16 := Del1_Base + Del1_Len - 1;
+
+   Dap2a_Base : constant U16 := Del1_Tail + 1;
+   Dap2a_Tail : constant U16 := Dap2a_Base + Dap2a_Len - 1;
+
+   Dap2b_Base : constant U16 := Dap2a_Tail + 1;
+   Dap2b_Tail : constant U16 := Dap2b_Base + Dap2b_Len - 1;
+
+   Del2_Base  : constant U16 := Dap2b_Tail + 1;
+   Del2_Tail  : constant U16 := Del2_Base + Del2_Len - 1;
+
+   Buffer_Size : constant U16 := Del2_Tail + 1;
+
+   type Reverb_Buffer is array (0 .. Buffer_Size - 1) of S16;
+
    type Instance
    is record
-      Buffer : Delay_Buffer (0 .. 16_384 - 1);
+      Buffer : Reverb_Buffer;
 
-      Amount : Param_Range := Param_Range (0.54 * 32_767.0);
-      Input_Gain : Param_Range := Param_Range (0.2 * 32_767.0);
+      Amount : Param_Range      := Param_Range (0.54 * 32_767.0);
+      Input_Gain : Param_Range  := Param_Range (0.2 * 32_767.0);
       Reverb_Time : Param_Range := Param_Range (0.35 * 32_767.0);
-      Diffusion : Param_Range := Param_Range (0.7 * 32_767.0);
-      LP : Param_Range := Param_Range ((0.6 + 0.37) * 32_767.0);
+      Diffusion : Param_Range   := Param_Range (0.7 * 32_767.0);
+      LP : Param_Range          := Param_Range ((0.6 + 0.37) * 32_767.0);
       LP_Decay_1 : S16 := 0;
       LP_Decay_2 : S16 := 0;
-
-      LFO_1, LFO_2 : Tresses.LFO.Instance;
 
       Write_Ptr : S16 := 0;
 
    end record;
 
-   procedure Reset (This : in out Instance);
-
-   procedure Process (This  : in out Instance;
-                      Left  : in out Mono_Buffer;
-                      Right : in out Mono_Buffer);
 
 end Tresses.FX.Reverb;
